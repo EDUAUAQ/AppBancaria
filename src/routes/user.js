21251e5f9680c972a942f316f1d5988ab41349d1
Andrei García <andreigar1017@gmail.com>
2024-09-11 20:50:07 -0600

    added main users requests

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,24 @@ const express = require('express');
 const user = express.Router();
 const db = require('../database/database');
 
+user.get("/profile/:user_id", async (req, res) => {
+    const { user_id } = req.params;
+
+    try {
+        const query = `SELECT user_id, username, email, first_name, last_name FROM users WHERE user_id = ${user_id}`;
+        const user = await db.query(query);
+
+        if (user.length > 0) {
+            return res.status(200).json({ code: 200, data: user[0] });
+        } else {
+            return res.status(404).json({ code: 404, message: "Usuario no encontrado" });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ code: 500, message: "Error en el servidor" });
+    }
+});
+
 
 user.post("/signup",async(req,res,next)=>{
     const {user_name, user_mail, user_password, first_name, last_name} = req.body;
@@ -29,5 +47,41 @@ user.post("/signup",async(req,res,next)=>{
     }
     return res.status(500).json({code:500, message:"Campos Incompletos"});
 });
+
+user.post("/login", async (req, res, next) => {
+    const { user_name, user_password } = req.body;
+
+    if (user_name && user_password) {
+        try {
+
+            let query = `SELECT user_id, password FROM users WHERE username = '${user_name}'`;
+
+            const rows = await db.query(query);
+
+
+            if (rows.length > 0) {
+                const user = rows[0];
+
+
+                if (user_password === user.password) { 
+
+                    // const token = jwt.sign({ userId: user.user_id }, 'secretKey', { expiresIn: '1h' });
+
+                    return res.status(200).json({ code: 200, message: "Inicio de sesión exitoso" });
+                } else {
+                    return res.status(401).json({ code: 401, message: "Contraseña incorrecta" });
+                }
+            } else {
+                return res.status(404).json({ code: 404, message: "Usuario no encontrado" });
+            }
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ code: 500, message: "Ocurrió un error en el servidor" });
+        }
+    }
+
+    return res.status(400).json({ code: 400, message: "Campos incompletos" });
+});
+
 
 module.exports = user;
