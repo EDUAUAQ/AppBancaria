348ec24111fd3723bbd78d11c00d5c1aa36ede0a
Andrei García <andreigar1017@gmail.com>
2024-10-22 18:16:50 -0600

    Added not logging to console

--- src/database/database.js
+++ src/database/database.js
@@ -5,6 +5,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',  // Cambia a 'postgres' si usas PostgreSQL
+    logging: false,
     define: {
         freezeTableName: true,  // Evita que Sequelize cambie el nombre de las tablas
     }
